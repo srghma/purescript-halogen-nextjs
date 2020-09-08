ef16bc67ce976b850379e19ab5bf6b9d6ee13f7b
Serhii Khoma <srghma@gmail.com>
2020-09-08 17:26:36 +0300

    feat: login -> WIP

--- app/Nextjs/PageImplementations/Login.purs
+++ app/Nextjs/PageImplementations/Login.purs
@@ -7,6 +7,8 @@ import Halogen.HTML as HH
 import Nextjs.AppM (AppM)
 import Nextjs.Link as Nextjs.Link
 import Nextjs.Route (Examples(..), Route(..))
+import HalogenMWC.TextField as TextField
+import HalogenMWC.Button as Button
 
 type Query = Const Void
 
@@ -14,7 +16,10 @@ type Input = Unit
 
 type Message = Void
 
-type State = Unit
+type State =
+  { email :: String
+  , password :: String
+  }
 
 type Action = Void
 
@@ -23,10 +28,18 @@ type ChildSlots = ()
 component :: H.Component Query Input Message AppM
 component =
   H.mkComponent
-    { initialState: const unit
-    , render
+    { initialState: const
+      { email: ""
+      , password: ""
+      }
     , eval: H.mkEval $ H.defaultEval
+    , render: \state ->
+      HH.div_
+        [ HH.text "Login"
+        , traceId $ TextField.filled TextField.defaultConfig
+        -- | , TextField.filled (TextField.defaultConfig { label = Just "email", value = Just state.email })
+        -- | , TextField.filled (TextField.defaultConfig { label = Just "password", value = Just state.password })
+        -- | , Button.button Button.Text Button.defaultConfig [ HH.text "Register" ]
+        -- | , Button.button Button.Raised Button.defaultConfig [ HH.text "Submit" ]
+        ]
     }
-
-render :: State -> HH.ComponentHTML Action ChildSlots AppM
-render _ = HH.div_ [ HH.text "Login" ]
