ab8cd7d3c1b958a9fadf046d5872320233163d1a
Serhii Khoma <srghma@gmail.com>
2020-12-06 18:52:49 +0200

    feat: without spy

--- packages/db-tests/tests/actions/really_create_user/success_not_verified.sql
+++ packages/db-tests/tests/actions/really_create_user/success_not_verified.sql
@@ -7,58 +7,33 @@ begin;
 
 select no_plan();
 
--- Spy
-
-select pgtest.spy(
-  'graphile_worker',
-  'add_job',
-  ARRAY['text', 'json', 'text', 'timestamp with time zone', 'integer', 'text', 'integer', 'text[]']::VARCHAR[]
-);
-
--- Spy END
-
-select graphile_worker.add_job(
-  'sendVerificationEmailForUserEmail',
-  json_build_object(
-    'id', 1
-  )::json
-);
-
 set local role :role;
 select is(current_user, :role);
 
-/* prepare actual as */
-/*   select username from app_private.really_create_user( */
-/*     username          => :username, */
-/*     email             => :email, */
-/*     email_is_verified => false, */
-/*     name              => null, */
-/*     avatar_url        => null, */
-/*     password          => :password */
-/*   ); */
+prepare actual as
+  select username from app_private.really_create_user(
+    username          => :username,
+    email             => :email,
+    email_is_verified => false,
+    name              => null,
+    avatar_url        => null,
+    password          => :password
+  );
 
-/* prepare expected as values */
-/*   (:username); */
+prepare expected as values
+  (:username);
 
-/* select set_eq( */
-/*   'actual', */
-/*   'expected' */
-/* ); */
-
--- Test body END
-
--- Check function has been called
+select set_eq(
+  'actual',
+  'expected'
+);
 
 set local role 'app_admin';
 
-/* select pgtest.assert_called( */
-/*   pgtest.get_mock_id( */
-/*     'graphile_worker', */
-/*     'add_job', */
-/*     ARRAY['text', 'json', 'text', 'timestamp with time zone', 'integer', 'text', 'integer', 'text[]'] */
-/*   ), */
-/*   1 */
-/* ); */
+select set_eq(
+  'select task_identifier from "graphile_worker".jobs',
+  ARRAY['sendVerificationEmailForUserEmail']
+);
 
 select finish();
 
